2215a8d41d79d6d9a55e329363df90dec1c0c67e
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2017-08-02 23:40:23 +0300

    add composer.json

---
+++ src/ModuleProvider.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace RabbitCMS\Payments\LiqPay;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ModuleProvider
+ *
+ * @package RabbitCMS\Payments\LiqPay
+ */
+class ModuleProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+}
